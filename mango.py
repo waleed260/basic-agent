272e5edf38b26feecf15b02773cbb828f86d7b31
Waleed Hassan <vkdeku20@gmail.com>
2026-05-08 21:40:09 +0500

    Update mango.py

--- mango.py
+++ mango.py
@@ -2,3 +2,27 @@
 
 
 
+
+from agents import AsyncOpenAI , OpenAIChatCompletionsModel,  Runner, Agent ,set_tracing_disabled
+from rich import print
+from dotenv import load_dotenv
+import os
+
+
+
+set_tracing_disabled(True)
+load_dotenv()
+
+key = os.getenv("GEMINI_API_KEY")
+base_url = os.getenv("BASE_URL")
+print(key)
+print(base_url)
+
+
+Gemini_client = AsyncOpenAI(api_key = key ,base_url= base_url)
+Model = OpenAIChatCompletionsModel(model = "gemini-2.5-flash", openai_client = Gemini_client)
+agent = Agent(name = "waleed" ,
+              instructions = "you are helpfull assistant ",
+              model = Model)
+res = Runner.run_sync(starting_agent = agent , input = "what is 2+18 = ?. give me answer in detail with emogis")
+print(res.final_output)
